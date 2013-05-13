8cb3723799b0dfca3ee6c5406a763363f5ae4805
Lucien XU <sfietkonstantin@free.fr>
2013-05-13 13:02:34 +0200

    Fixed autoobs

--- autoobs.sh
+++ autoobs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cp qtc_packaging/debian_harmattan debian -R
 cp debian/rules-obs debian/rules -f
-/opt/QtSDK/Madde/bin/mad -t harmattan_10.2011.34-1_rt1.2 dpkg-buildpackage -sa -S -uc -us -Imoc -Iobj -Ircc -Iui -I.svn -I*.deb -I*.changes -Iqtc_packaging -IMakefile -I*.pro.user
+/opt/Qt/QtSDK/Madde/bin/mad -t harmattan_10.2011.34-1_rt1.2 dpkg-buildpackage -sa -S -uc -us -Imoc -Iobj -Ircc -Iui -I.svn -I*.deb -I*.changes -Iqtc_packaging -IMakefile -I*.pro.user -Ibuild -Iinstall
 rm debian -R
